59ea1ba3a06349db9396536003475ac62a7e1d14
bhardwaj-hrishabh <bhardwajhrishabh07@gmail.com>
2024-10-13 12:07:24 +0530

    all changes saved

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,7 +9,7 @@ import axios from 'axios';
 import { Toaster } from 'react-hot-toast';
 
 
-axios.defaults.baseURL = "http://localhost:5000/api/v1";
+axios.defaults.baseURL = "https://fusiongpt-8w8z.onrender.com";
 axios.defaults.withCredentials = true;
 
 const theme = createTheme({
