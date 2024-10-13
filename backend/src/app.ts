3eb5fe2aa66352852d2b1563ae63257b3a3cb0fa
bhardwaj-hrishabh <bhardwajhrishabh07@gmail.com>
2024-10-13 14:57:27 +0530

    Changes Added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,5 @@
 import express from "express"
 import {config} from "dotenv"
-import morgan from "morgan"
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -23,8 +22,7 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-// remove it in production
-app.use(morgan("dev"));
+
 
 
 app.use("/api/v1", appRouter);
